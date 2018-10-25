fb2c3a6f88b8d5d9ec9ace689e1861d6d06ed9a0
takeshi.sugano <zhr55475@gmail.com>
2018-10-25 05:56:17 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,8 @@
 */
 
 Route::get('/', 'TasksController@index');
+
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
 Route::resource('tasks','TasksController');
